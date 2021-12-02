aa3557aa8062107e2e740a675ab03c059f4fbc54
duanyytop <duanyytop@gmail.com>
2021-12-02 14:26:07 +0800

    refactor: Handle request parameters error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::utils::{check_request_params_error, values_to_u8_vec};
+use crate::utils::{check_request_params, parse_values};
+use jsonrpc_http_server::jsonrpc_core::serde_json::Map;
 use jsonrpc_http_server::jsonrpc_core::{IoHandler, Params, Value};
 use jsonrpc_http_server::ServerBuilder;
 use smt::generate_registry_smt;
@@ -12,18 +13,19 @@ fn main() {
         match params {
             Params::Array(values) => {
                 println!("params: {:?}", values);
-                if check_request_params_error(values.clone()) {
-                    return Ok(Value::String("Request parameters error".to_owned()));
+                if let Some(error) = check_request_params(values.clone()) {
+                    return Ok(error);
                 }
-                let lock_hashes = values_to_u8_vec(values);
+                let lock_hashes = parse_values(values);
                 let (root_hash, witness_data) = generate_registry_smt(lock_hashes);
 
-                Ok(Value::Array(vec![
-                    Value::String(root_hash),
-                    Value::String(witness_data),
-                ]))
+                let mut response = Map::new();
+                response.insert("registry_entries".to_string(), Value::String(witness_data));
+                response.insert("smt_root_hash".to_string(), Value::String(root_hash));
+
+                Ok(Value::Object(response))
             }
-            _ => Ok(Value::String("Parameters error".to_owned())),
+            _ => Ok(Value::String("Request parameter format error".to_owned())),
         }
     });
 
--- src/utils.rs
+++ src/utils.rs
@@ -8,7 +8,7 @@ fn remove_0x(str: &str) -> &str {
     }
 }
 
-pub fn values_to_u8_vec(values: Vec<Value>) -> Vec<[u8; 32]> {
+pub fn parse_values(values: Vec<Value>) -> Vec<[u8; 32]> {
     values
         .iter()
         .map(|value| {
@@ -22,27 +22,39 @@ pub fn values_to_u8_vec(values: Vec<Value>) -> Vec<[u8; 32]> {
         .collect()
 }
 
-pub fn check_request_params_error(values: Vec<Value>) -> bool {
+pub fn check_request_params(values: Vec<Value>) -> Option<Value> {
     for value in values.iter() {
         if !value.is_string() {
-            return true;
+            return Some(Value::String(
+                "Request parameters must be string".to_string(),
+            ));
         }
-        let hex_str = remove_0x(value.as_str().unwrap());
+        let hex_str = value.as_str().unwrap();
+        if !hex_str.contains("0x") {
+            return Some(Value::String(
+                "Request parameters must be prefixed with 0x".to_string(),
+            ));
+        }
+        let hex_str = remove_0x(hex_str);
         if hex_str.len() != 64 {
-            return true;
+            return Some(Value::String(
+                "Request parameters must be 32 bytes".to_string(),
+            ));
         }
         if hex::decode(hex_str).is_err() {
-            return true;
+            return Some(Value::String(
+                "Request parameters must be hex string".to_string(),
+            ));
         }
     }
-    false
+    None
 }
 
 #[test]
-fn test_values_to_u8_vec() {
+fn test_parse_values() {
     let hex = "0xea28c98f38b4a57aa81756b167bb37fa42daf67edbc9863afb8172096ed301c2";
     let values = vec![Value::String(hex.to_string())];
-    let vec = values_to_u8_vec(values);
+    let vec = parse_values(values);
     let result: Vec<[u8; 32]> = vec![[
         234, 40, 201, 143, 56, 180, 165, 122, 168, 23, 86, 177, 103, 187, 55, 250, 66, 218, 246,
         126, 219, 201, 134, 58, 251, 129, 114, 9, 110, 211, 1, 194,
@@ -55,7 +67,7 @@ fn test_check_request_params() {
     let values = vec![Value::String(
         "0xea28c98f38b4a57aa81756b167bb37fa42daf67edbc9863afb8172096ed301c2".to_string(),
     )];
-    assert!(!check_request_params_error(values));
+    assert!(!check_request_params(values).is_none());
 
     let values = vec![
         Value::String(
@@ -64,5 +76,5 @@ fn test_check_request_params() {
         ),
         Value::String("0x28c98f38b4a57aa81756b167bb37fa42daf67edbc9863afb8172096e".to_string()),
     ];
-    assert!(check_request_params_error(values));
+    assert!(check_request_params(values).unwrap() == "Request parameters must be 32 bytes");
 }

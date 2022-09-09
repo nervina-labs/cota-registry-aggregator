53c10d56344c1e9bb6d0ca50c2298cf20fcf0ef4
Dylan <duanyytop@gmail.com>
2022-09-09 12:02:18 +0800

    feat: Update diesel schema

--- src/schema.rs
+++ src/schema.rs
@@ -14,6 +14,7 @@ table! {
         id -> Bigint,
         block_number -> Unsigned<Bigint>,
         lock_hash -> Char,
+        cota_cell_id -> Unsigned<Bigint>,
         created_at -> Datetime,
         updated_at -> Datetime,
     }
@@ -26,4 +27,9 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(check_infos, register_cota_kv_pairs, schema_migrations,);
+
+allow_tables_to_appear_in_same_query!(
+    check_infos,
+    register_cota_kv_pairs,
+    schema_migrations,
+);
